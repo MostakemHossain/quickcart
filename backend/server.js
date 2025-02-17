637f60ec96d667ba1828217230d69bb0b8561304
MostakemHossain <mostakememon123@gmail.com>
2025-02-17 21:51:15 +0600

    databse initilization

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,41 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import productRoutes from './routes/productRoutes.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { sql } from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
-const app= express();
+const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/products", productRoutes);
 
-app.use("/api/products",productRoutes);
-app.listen(PORT,()=>{
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        
+        `;
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+  }
+}
+initDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+  });
+});
